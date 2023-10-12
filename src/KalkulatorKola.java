d758df3141859a46fc0e44419bb60ad832cd3b34
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-10-12 22:22:04 +0200

    Dodano wczytanie i obliczanie trójkąta

--- src/KalkulatorKola.java
+++ src/KalkulatorKola.java
@@ -3,16 +3,32 @@ public class KalkulatorKola {
     public static void main(String[] args) {
        double pi = 3.14;
 
+       /* koło */
        Scanner scan = new Scanner(System.in);
        System.out.println("Podaj promien koła w [cm]");
        double promienKola = scan.nextDouble();
 
        //pole = pi * (r*r)
-       double pole = pi * Math.pow(promienKola,2); // Math. to funkcje matematyczne
-       double obwod = (2 * pi) * promienKola;
+       double poleKola = pi * Math.pow(promienKola,2); // Math. to funkcje matematyczne
+       double obwodKola = (2 * pi) * promienKola;
 
-       System.out.println("Obwód koła to: " + obwod + " cm");
-       System.out.println("Pole koła to: " + pole + "  cm^2");
+
+       /* trójkąt równoboczny */
+       System.out.println("Podaj długość boku trójkąta równobocznego w [cm]");
+       double podstawaTrojkata = scan.nextDouble();
+       System.out.println("Podaj wysokość trójkąta w [cm]");
+       double wysokoscTrojkata = scan.nextDouble();
+
+       double poleTrojkata = (podstawaTrojkata * wysokoscTrojkata)/2;
+       double obwodTrojkata = podstawaTrojkata * 3;
+
+
+
+       System.out.println("Obwód koła to: " + obwodKola + " cm");
+       System.out.println("Pole koła to: " + poleKola + "  cm^2");
+
+       System.out.println("Pole trójkąta to: " + poleTrojkata + " cm^2");
+       System.out.println("Obwód trójkąta to: " + obwodTrojkata + "  cm");
 
     }
 }
