498cebd40f43ddf451b90e33e664fd8101732065
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-10-26 11:54:36 +0200

    Nowy branch jtp

--- src/test.java
+++ src/test.java
@@ -1,19 +1,10 @@
+import java.util.Scanner;
 public class test {
     public static void main(String[] args) {
-        int x = 5;
-        int y = 5;
-        int a = x++;
-        int b = ++y;
-        int c = ++x;
-        int d = ++x;
-        int e = ++x;
+        System.out.println("Podaj słowo");
+        String slowo = new Scanner(System.in).next();
 
-        System.out.println(x);
-        System.out.println(a);
-        System.out.println(y);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
+        System.out.println(slowo.toUpperCase());
+
     }
 }
