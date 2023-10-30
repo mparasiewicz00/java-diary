30a68f5069f6d5f33fc76422f0be71be89eaba0e
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-10-30 12:51:11 +0100

    Dzienny update

--- src/test.java
+++ src/test.java
@@ -1,19 +1,11 @@
 public class test {
     public static void main(String[] args) {
         int x = 5;
-        int y = 5;
-        int a = x++;
-        int b = ++y;
-        int c = ++x;
-        int d = ++x;
-        int e = ++x;
 
-        System.out.println(x);
-        System.out.println(a);
-        System.out.println(y);
-        System.out.println(b);
-        System.out.println(c);
-        System.out.println(d);
-        System.out.println(e);
+        boolean tof;
+
+        tof = x  == 0;
+
+        System.out.println("X jest większe lub równe zeru: " + tof);
     }
 }
