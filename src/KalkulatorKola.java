443a42769b2e786a002613e9ee30f0ba9b09df60
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-11-20 13:33:49 +0100

    Modyfikacja kalkulatora - instrukcje warunkowe

--- src/KalkulatorKola.java
+++ src/KalkulatorKola.java
@@ -1,34 +1,65 @@
 import java.util.Scanner;
 public class KalkulatorKola {
     public static void main(String[] args) {
-       double pi = 3.14;
+       final double pi = 3.14;
+       String figura;
+       String operacja;
+       boolean nieprawidlowaOperacja = false;
 
-       /* koło */
-       Scanner scan = new Scanner(System.in);
-       System.out.println("Podaj promien koła w [cm]");
-       double promienKola = scan.nextDouble();
+       /* pobranie operacji */
 
-       //pole = pi * (r*r)
-       double poleKola = pi * Math.pow(promienKola,2); // Math. to funkcje matematyczne
-       double obwodKola = (2 * pi) * promienKola;
-
-
-       /* trójkąt równoboczny */
-       System.out.println("Podaj długość boku trójkąta równobocznego w [cm]");
-       double podstawaTrojkata = scan.nextDouble();
-       System.out.println("Podaj wysokość trójkąta w [cm]");
-       double wysokoscTrojkata = scan.nextDouble();
-
-       double poleTrojkata = (podstawaTrojkata * wysokoscTrojkata)/2;
-       double obwodTrojkata = podstawaTrojkata * 3;
+       System.out.println("Podaj figurę, której chcesz uruchomić kalkulator: T dla trójkąta lub K dla koła ");
+       figura = getString();
 
 
+       if (figura.equals("T")){
+          System.out.println("Podaj długość boku trójkąta równobocznego w [cm]");
+          double podstawaTrojkata = getDouble();
+          System.out.println("Podaj wysokość trójkąta w [cm]");
+          double wysokoscTrojkata = getDouble();
+          System.out.println("Podaj operację do wykonania: P jeżeli pole lub O jeżeli obwód ");
+          operacja = getString();
+          if(operacja.equals("P")){
+             double poleTrojkata = (podstawaTrojkata * wysokoscTrojkata)/2;
+             System.out.println("Obwód trójkąta to:  " + poleTrojkata + "  cm");
+          } else if(operacja.equals("O")) {
+             double obwodTrojkata = podstawaTrojkata * 3;
+             System.out.println("Obwód trójkąta to:  " + obwodTrojkata + "  cm");
+          } else {
+             nieprawidlowaOperacja = true;
+             System.out.println("Nieprawidłowa operacja!");
+          }
 
-       System.out.println("Obwód koła to: " + obwodKola + " cm");
-       System.out.println("Pole koła to: " + poleKola + "  cm^2");
 
-       System.out.println("Pole trójkąta to:  " + poleTrojkata + " cm^2");
-       System.out.println("Obwód trójkąta to:  " + obwodTrojkata + "  cm");
+
+       } else if(figura.equals("K")) {
+          System.out.println("Podaj promien koła w [cm]");
+          double promienKola = getDouble();
+          System.out.println("Podaj operację do wykonania: P jeżeli pole lub D jeżeli długość ");
+          operacja = getString();
+
+          if(operacja.equals("P")){
+             double poleKola = pi * Math.pow(promienKola,2); // Math. to funkcje matematyczne
+             System.out.println("Pole koła to: " + poleKola + "  cm^2");
+          } else if (operacja.equals("D")){
+             double dlugoscKola = (2 * pi) * promienKola;
+             System.out.println("Obwód koła to: " + dlugoscKola + " cm");
+          } else {
+             nieprawidlowaOperacja = true;
+             System.out.println("Nieprawidłowa operacja!");
+          }
+       } else {
+          nieprawidlowaOperacja = true;
+          System.out.println("Nieprawidłowy symbol figury!");
+       }
 
     }
+
+   public static double getDouble() {
+      return new Scanner(System.in).nextDouble();
+   }
+
+   public static String getString() {
+      return new Scanner(System.in).next();
+   }
 }
