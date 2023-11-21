aa7b5246fbb2c5fdb156959f177f62dfa3f6c869
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-11-21 11:56:51 +0100

    Dzielenie przez trzy

---
+++ src/dzielPrzezTrzy.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class dzielPrzezTrzy {
+    public static void main(String[] args) {
+        int x;
+
+        System.out.println("Podaj liczbe");
+        x = getInt();
+
+        if(!(x%3 ==0)) {
+            System.out.println("Liczba nie jest podzielna przez zero");
+        } else {
+            System.out.println("Liczba jest podzielna przez zero");
+        }
+
+
+    }
+    private static int getInt(){
+        return new Scanner(System.in).nextInt();
+    }
+
+}
