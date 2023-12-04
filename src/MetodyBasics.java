ac684ca322aea1008fa711eeb5b8f07973866526
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-12-04 15:32:55 +0100

    Dodanie nowych metod

--- src/MetodyBasics.java
+++ src/MetodyBasics.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class MetodyBasics {
     public static void main(String[] args) {
         welcome();
 
         System.out.println(subtraction(10,3));
+
+        System.out.println("Podaj liczbę, a powiem ci jaki jest jej kwadrat!");
+        System.out.println("Kwadrat podanej liczby to: " + sqrt(getInt()));
+
+        stars(10);
+
+    }
+
+
+    public static int getInt(){ return new Scanner(System.in).nextInt();
     }
     public static void welcome() {
         System.out.println("Hello!");
@@ -10,4 +22,16 @@ public class MetodyBasics {
     public static int subtraction(int x, int y) {
         return x - y;
     }
+
+    public static int sqrt(int number) {
+        return number * number;
+    }
+    
+    public static void stars(int stars) {
+        for(int i = 0; i <= stars; i++ ){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    
 }
