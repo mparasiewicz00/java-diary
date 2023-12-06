1023cec17e4e384fcfed07514b8c86ac6bdbee49
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-12-06 23:11:16 +0100

    Zadania metody(argumenty metod)

--- src/MetodyBasics.java
+++ src/MetodyBasics.java
@@ -11,8 +11,19 @@ public class MetodyBasics {
 
         stars(10);
 
-    }
+        System.out.println( lastSign("witaj"));
 
+
+        ifPalindrom("kot");
+
+
+        int[] tabTest = {1,2,3,4,5};
+        System.out.println("Suma liczb w tablicy to: " +tabSum(tabTest));
+
+
+        System.out.println(sumSigns("Ala ma kota", 'a'));
+
+    }
 
     public static int getInt(){ return new Scanner(System.in).nextInt();
     }
@@ -33,5 +44,48 @@ public class MetodyBasics {
         }
         System.out.println();
     }
+
+    public static String lastSign(String input) {
+
+        return String.valueOf(input.charAt(input.length() -1));
+    }
     
+    public static void ifPalindrom(String word){
+        int dlugosc = word.length();
+        boolean ifpali = true;
+
+        for (int i = 0; i < dlugosc / 2; i++) {
+            if (word.charAt(i) != word.charAt(dlugosc - 1 - i)) {
+                ifpali = false;
+                break;
+            }
+        }
+
+        if (ifpali) {
+            System.out.println("Slowo " + word + " jest palindromem.");
+        } else {
+            System.out.println("Slowo " + word + " nie jest palindromem.");
+        }
+    }
+
+    public static int tabSum(int[] tab) {
+        int summedUp = 0;
+        for (int j : tab) {
+            summedUp += j;
+        }
+        return summedUp;
+    }
+
+    public static int sumSigns(String word, char signToSum) {
+        int sumOfSigns = 0;
+
+        for (int i = 0; i <= (word.length() -1); i++){
+            char checkedSign = word.charAt(i);
+            if (checkedSign == signToSum){
+                sumOfSigns++;
+            }
+        }
+    return sumOfSigns;
+    }
+
 }
