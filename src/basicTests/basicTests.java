88ef5aae8777552ff83812ae73fb22363bb964ca
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-12-09 19:07:15 +0100

    Dodanie zmian

--- src/basicTests/basicTests.java
+++ src/basicTests/basicTests.java
@@ -1,29 +1,26 @@
+package basicTests;
+
 public class basicTests {
     public static void main(String[] args) {
         square_positive_squaredValue();
         square_negative_squaredValue();
         square_zero_zero();
-
     }
-    public static int square (int number){
-        return number *= number;
-    }
-
     public static void square_positive_squaredValue(){
         int number = 10;
-        int result = square(number);
+        int result = tested.square(number);
         assertEquals(100, result);
     }
 
     public static void square_negative_squaredValue(){
         int number = -5;
-        int result = square(number);
+        int result = tested.square(number);
         assertEquals(25, result);
     }
 
     public static void square_zero_zero(){
         int number = 0;
-        int result = square(number);
+        int result = tested.square(number);
         assertEquals(0, result);
     }
 
---
+++ src/basicTests/tested.java
@@ -0,0 +1,7 @@
+package basicTests;
+
+public class tested {
+    public static int square (int number){
+        return number *= number;
+    }
+}
