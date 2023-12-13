a798f37875a6ef02e246e19eede8e86aceb48596
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-12-13 19:14:29 +0100

    Adding class Point

---
+++ src/Point.java
@@ -0,0 +1,28 @@
+public class Point {
+    private int x, y;
+
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
