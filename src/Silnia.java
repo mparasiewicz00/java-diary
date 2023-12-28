03a3e6e8f697c1d33a6d81f8f4ae4fafdbce39f7
Mateusz Parasiewicz <m.parasiewicz00@gmail.com>
2023-12-28 21:29:56 +0100

    Trying to add exceptions

--- src/Silnia.java
+++ src/Silnia.java
@@ -1,9 +1,14 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Silnia {
     public static void main(String[] args) {
 
         System.out.println("Podaj liczbę, której chcesz obliczyć silnię");
-        factorial(getInt());
+        try {
+            factorial(getInt());
+        } catch (InputMismatchException e){
+            System.out.println("To nie jest liczba! Błąd: " + e.getMessage());
+        }
 
     }
     private static int getInt(){
@@ -16,7 +21,7 @@ public class Silnia {
             for(int i = 1;i <= liczba; i++){
                 wynik *= i;
             }
-            System.out.println("Silnia dla liczby " + liczba + "to: " + wynik);
+            System.out.println("Silnia dla liczby " + liczba + " to: " + wynik);
         } else {
             System.out.println("Silnia dla 0 to 1");
         }
