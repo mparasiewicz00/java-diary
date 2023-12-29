c909ef4c4ab68802d94518f1aca631c3da13e53f
Mateusz Parasiewicz <mateuszparasiewicz@macbook-air-mateusz.home>
2023-12-30 00:39:14 +0100

    Test commit new device

--- src/CoDrugiElemTab.java
+++ src/CoDrugiElemTab.java
@@ -4,10 +4,9 @@ public class CoDrugiElemTab {
         int[] pierwsza = {1,2,3,4,5,6,7,8,9,10};
         int[] druga = {1,2,3,4,5,6,7,8,9,10,11,12,13};
 
-        for (int i = 0; i < pierwsza.length; i += 2){
+        for (int i = 0; i < pierwsza.length; i += 2) {
             System.out.print(pierwsza[i] + ", ");
         }
-
         System.out.println();
 
         for (int i = 0; i < druga.length; i += 2){
