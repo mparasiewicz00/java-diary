49eef8252e4998f0091a2fe0e888c27de8c43c09
mateuszparasiewicz <m.parasiewicz00@gmail.com>
2024-01-01 21:26:42 +0100

    Changes in Personal

--- src/Personal/Personal.java
+++ src/Personal/Personal.java
@@ -5,20 +5,46 @@ public class Personal {
     private String firstName;
     private String secondName;
 
-    public void newAge(int ageValue) throws IllegalAgeException {
-        if (ageValue <=0 ) {
-            throw new IllegalAgeException("Wiek musi być większy od 0!");
+
+    public Personal (int age, String firstName, String secondName) throws IllegalArgumentException {
+        if (age <=0 ) {
+            throw new IllegalArgumentException("Wiek musi być większy od 0!");
         }
-        age = ageValue;
+        if ( firstName == null ) {
+            throw new IllegalArgumentException("Nie podano imienia.");
+        }
+        if (secondName == null ) {
+            throw new IllegalArgumentException("Nie podano nazwiska.");
+        }
+        this.age = age;
+        this.firstName = firstName;
+        this.secondName = secondName;
     }
 
-    public void setFirstName(String nameOfPerson){
-        firstName = nameOfPerson;
+    public int getAge() {
+        return age;
     }
 
-    public void setSecondName(String secNameOfPerson){
-        secondName = secNameOfPerson;
+    public String getFirstName() {
+        return firstName;
     }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
     @Override
     public String toString() {
         return "Personal.Personal{" +
--- src/Personal/PersonalUsage.java
+++ src/Personal/PersonalUsage.java
@@ -3,17 +3,12 @@ package Personal;
 public class PersonalUsage {
     public static void main(String[] args) {
 
-        Personal personA = new Personal();
-
         try {
-            personA.newAge(-1);
-        } catch (IllegalAgeException e) {
-            System.out.println("Wystąpił błąd! Komunikat błędu: " + e.getMessage());
+            Personal person = new Personal(22,"Adam", "Abacki");
+            System.out.println(person);
+        } catch (IllegalArgumentException e){
+            System.out.println("Wystąpił błąd. Komunikat błędu: " + e.getMessage());
         }
-        personA.setFirstName("Mateusz");
-        personA.setSecondName("Parasiewicz");
-
-        System.out.println(personA);
     }
 }
 
