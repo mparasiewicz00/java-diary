8b696255f46823ae387092821ee50590cd4bbc08
mateuszparasiewicz <m.parasiewicz00@gmail.com>
2024-01-01 22:27:44 +0100

    Last daily update

--- src/Personal/PersonalUsage.java
+++ src/Personal/PersonalUsage.java
@@ -4,8 +4,14 @@ public class PersonalUsage {
     public static void main(String[] args) {
 
         try {
-            Personal person = new Personal(22,"Adam", "Abacki");
-            System.out.println(person);
+            Personal personA = new Personal(22,null, "Abacki");
+            System.out.println(personA);
+        } catch (IllegalArgumentException e){
+            System.out.println("Wystąpił błąd. Komunikat błędu: " + e.getMessage());
+        }
+        try {
+            Personal personB = new Personal(-1,"Adam", "Abacki");
+            System.out.println(personB);
         } catch (IllegalArgumentException e){
             System.out.println("Wystąpił błąd. Komunikat błędu: " + e.getMessage());
         }
